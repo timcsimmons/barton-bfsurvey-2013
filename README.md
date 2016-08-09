6efad7b376aefcdf8b78ccceb0ae64baf06c4b2b
timcsimmons <timothony@gmail.com>
2016-08-09 12:04:33 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Milk supply/breastfeeding survey
+
+Repository of documentation and programs for the milk
+supply/breastfeeding survey
---
+++ programs/1-analysis-db.sql
@@ -0,0 +1,15 @@
+------------------------------------------------------------------------
+-- Program: 1-analysis-db.sql
+-- Directory: bfsurvey/programs
+-- Project: Breastfeeding survey analysis
+-- Author: Tim Simmons
+--
+-- Purpose:
+-- Notes:
+--   Run from a temporary sqlite3 session in this directory using
+--   .read 1-analysis-db.sql
+------------------------------------------------------------------------
+
+.header on
+.mode column
+
